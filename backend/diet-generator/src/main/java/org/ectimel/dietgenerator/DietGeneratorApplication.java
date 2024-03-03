698eb35f9ee2b17158ea6e86ab40c8ea485d2110
Dominik Tworek <dtworek94@gmail.com>
2024-03-03 23:46:29 +0000

    Created NinjaResponse and NinjaItem and checked if data is fetched properly.

--- backend/diet-generator/src/main/java/org/ectimel/dietgenerator/DietGeneratorApplication.java
+++ backend/diet-generator/src/main/java/org/ectimel/dietgenerator/DietGeneratorApplication.java
@@ -1,13 +1,42 @@
 package org.ectimel.dietgenerator;
 
+import org.ectimel.dietgenerator.generator.NinjaResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
+
+import java.util.Objects;
 
 @SpringBootApplication
-public class DietGeneratorApplication {
+public class DietGeneratorApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DietGeneratorApplication.class, args);
-	}
+    @Value("${ninja-api-key}")
+    private String ninjaApiKey;
 
+    public static void main(String[] args) {
+        SpringApplication.run(DietGeneratorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        RestClient restClient = RestClient.create();
+
+        String baseURI = "https://api.calorieninjas.com/v1/nutrition?query=";
+        String query = "potato";
+        String URI = baseURI + query;
+
+        NinjaResponse ninjaResponse = restClient.get()
+                .uri(URI)
+                .header("X-Api-Key", ninjaApiKey)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(NinjaResponse.class);
+
+        assert ninjaResponse != null;
+        ninjaResponse.getItems().forEach(ninjaItem -> System.out.println(ninjaItem.calories));
+    }
 }
---
+++ backend/diet-generator/src/main/java/org/ectimel/dietgenerator/generator/NinjaResponse.java
@@ -0,0 +1,10 @@
+package org.ectimel.dietgenerator.generator;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class NinjaResponse {
+    List<NinjaItem> items;
+}
