5f8e3681bc3931b31dd6b053e9bd16825eb14f77
Dominik Tworek <dtworek94@gmail.com>
2024-04-28 18:10:01 +0100

    Added unit tests for BMICalculator

--- backend/diet-generator/src/main/java/org/ectimel/dietgenerator/domain/calculator/bmi/BMICalculator.java
+++ backend/diet-generator/src/main/java/org/ectimel/dietgenerator/domain/calculator/bmi/BMICalculator.java
@@ -7,6 +7,9 @@ import java.math.RoundingMode;
 public interface BMICalculator {
 
     static BigDecimal calculate(BigDecimal bodyWeightInKg, BigDecimal heightInCm) {
+        if(bodyWeightInKg == null || bodyWeightInKg.doubleValue() <= 0 || heightInCm == null || heightInCm.doubleValue() <= 0)
+            return BigDecimal.valueOf(0);
+
         BigDecimal heightInMeters = heightInCm.divide(new BigDecimal("100"), new MathContext(3, RoundingMode.HALF_DOWN));
         BigDecimal heightCubed = heightInMeters.multiply(heightInMeters, new MathContext(3, RoundingMode.HALF_DOWN));
         return bodyWeightInKg.divide(heightCubed, new MathContext(3, RoundingMode.HALF_UP));
