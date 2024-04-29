84a1b5b4a78fe45c749801d23a36c87ef300593a
Dominik Tworek <dtworek94@gmail.com>
2024-04-30 00:47:39 +0100

    Added tests for BaseMetabolicRate calculateTDEE method.

--- backend/diet-generator/src/main/java/org/ectimel/dietgenerator/domain/calculator/calories/BaseMetabolicRate.java
+++ backend/diet-generator/src/main/java/org/ectimel/dietgenerator/domain/calculator/calories/BaseMetabolicRate.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 @Getter
 @AllArgsConstructor
@@ -11,7 +13,8 @@ public class BaseMetabolicRate {
     private BigDecimal baseBMR;
 
     public BigDecimal calculateTDEE(ActiveLevel activeLevel) {
+        if(activeLevel == null) return BigDecimal.valueOf(0);
         BigDecimal multiplayer = BigDecimal.valueOf(activeLevel.getMultiplayer());
-        return multiplayer.multiply(baseBMR);
+        return multiplayer.multiply(baseBMR).setScale(2, RoundingMode.HALF_DOWN);
     }
 }
