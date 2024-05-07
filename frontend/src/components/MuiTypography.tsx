6abfe5ea81f6172b10d9fb2f27819f277ae512e1
Dominik Tworek <dtworek94@gmail.com>
2024-05-07 03:16:13 +0100

    Added Material UI library to frontedn app, and created test component

---
+++ frontend/src/components/MuiTypography.tsx
@@ -0,0 +1,24 @@
+import {Typography} from "@mui/material";
+
+export const MuiTypography = () => {
+
+    return (
+        <div>
+            <Typography variant="h1">h1 Heading</Typography>
+            <Typography variant="h2">h2 Heading</Typography>
+            <Typography variant="h3">h3 Heading</Typography>
+            <Typography variant="h4">h4 Heading</Typography>
+            <Typography variant="h5">h5 Heading</Typography>
+            <Typography variant="h6">h6 Heading</Typography>
+
+            <Typography variant="subtitle1">Subtitle 1</Typography>
+            <Typography variant="subtitle2">Subtitle 2</Typography>
+
+            <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut commodi cumque expedita fugit laborum, magnam nam nemo quam, quidem quo repellendus repudiandae ullam! Minima praesentium quibusdam quis voluptate voluptatum.</Typography>
+
+
+            <Typography variant="body2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut commodi cumque expedita fugit laborum, magnam nam nemo quam, quidem quo repellendus repudiandae ullam! Minima praesentium quibusdam quis voluptate voluptatum.</Typography>
+
+        </div>
+    )
+}
