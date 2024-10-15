6f3e5753234f7a40bee398f6055024b139d97e37
ecti <dtworek94@gmail.com>
2024-10-15 23:09:57 +0100

    up

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosBase = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
+    baseURL: "http://localhost:8181/api/v1",
     headers: {'Content-Type': 'application/json'},
     timeout: 10000,
 })
