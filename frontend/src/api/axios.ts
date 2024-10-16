6940f747d955af9e6e6e4a89795aeb19e5174c11
ecti <dtworek94@gmail.com>
2024-10-16 15:15:36 +0100

    changed api link

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosBase = axios.create({
-    baseURL: "http://127.0.0.1/api/v1",
+    baseURL: "https://fatatu.com/api/v1",
     headers: {'Content-Type': 'application/json'},
     timeout: 10000,
 })
